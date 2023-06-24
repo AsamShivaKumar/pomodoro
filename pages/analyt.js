66bac80b5ac5c11956e3ad664191432883fe8f12
AsamShivaKumar <asamshivakumar@gmail.com>
2023-06-24 20:08:55 +0530

    Modified analytics

--- pages/analyt.js
+++ pages/analyt.js
@@ -70,7 +70,7 @@ export default function Analytics() {
                         </>
                     }
                     {
-                        userObj.weekStats.every(x => (x == 0)) && <h1 className="barChart" style={{padding: "20px 20px"}}>Earn tomatoes to see the analysis</h1>
+                        userObj.weekStats.every(x => (x == 0)) && <h1 className="barChart" style={{padding: "20px 20px", height: "200px" }}>Earn tomatoes to see the analysis</h1>
                     }
                 </div>
             }
